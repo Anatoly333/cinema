0221f1e8d9c80e09e23e0bcba99e015fee3304e5
Anatoly333 <45653949+Anatoly333@users.noreply.github.com>
2019-10-28 20:39:03 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -1,6 +1,17 @@
-'''Этот класс создан для работы с hnml'''
+import requests
+  ''
+'Этот класс создан для работы с hnml'
+''
 class CinemaParser():
-    '''Самый полезный класс'''
-    def __init__(self, city='msk'):
-        '''"Это функция получает на вход город (Москва или Санкт-Петербург)'''
-        self.city = city
+  ''
+'Самый полезный класс'
+''
+def __init__(self, city = 'msk'):
+  ''
+'"Это функция получает на вход город (Москва или Санкт-Петербург)'
+''
+self.city = city
+def extract_raw_content():
+  self.content = requests.get('https://msk.subscity.ru')
+def print_raw_content():
+  print(self.content)
